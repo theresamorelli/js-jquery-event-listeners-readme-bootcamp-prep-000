e245e4490955232322ebea0441d799e3f81c9f4c
Theresa <theresa.l.morelli@gmail.com>
2018-05-07 19:03:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,25 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => alert('Hey!'));
+}
+
+function frameIt() {
+  $('img').on('load', () => $('img').addClass('tasty'));
+}
+
+function pressIt() {
+  $('#typing').on('keydown', (key) => {
+    if (key.which === 71) alert('You have pressed the G key');
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', () => alert('Your form is going to be submitted now.'));
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
