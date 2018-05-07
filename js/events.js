f395f1c7eab3aaa52d24a175e3b7e00027fdffc3
Theresa <theresa.l.morelli@gmail.com>
2018-05-07 18:47:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,13 +1,13 @@
 //define functions here
-function getIt() {
-  $('p').on('click', () => {
-    alert('Hey!');
-  })
-}
-
-function frameIt() {
-  
-}
+// function getIt() {
+//   $('p').on('click', () => {
+//     alert('Hey!');
+//   })
+// }
+//
+// function frameIt() {
+//
+// }
 
 $(document).ready(function(){
 
