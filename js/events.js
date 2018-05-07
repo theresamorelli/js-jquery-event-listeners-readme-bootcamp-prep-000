7ab68172538e0e01586ad7f762d775828651fbbd
Theresa <theresa.l.morelli@gmail.com>
2018-05-07 18:59:58 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,6 +7,12 @@ function frameIt() {
   $('img').on('load', () => $('img').addClass('tasty'));
 }
 
+function pressIt() {
+  $('#typing').on('keydown', (key) => {
+    if (key.which === 71) alert('You have pressed the G key');
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
